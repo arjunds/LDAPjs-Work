3ca9e92d1da51231faba596a40b27cf63587d21d
arjun333 <arjundsampath@gmail.com>
2016-06-06 18:46:55 -0700

    Did Stuff

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
+var ldap = require('ldapjs'),
+    http = require('http'),
+    client = ldap.createClient({
+        url: 'ldap://127.0.0.1:1389'
+    });
 
+var ldapServer = ldap.createServer();
+ldapServer.listen(8081, 'localhost', function() {
+    console.log('LDAP server up at: %s', ldapServer.url);
+});
+
+var server = http.createServer(function(req, res) {
+
+});
+server.listen(8080, 'localhost', function() {
+    console.log('Server running at: %s', server.url)
+});
